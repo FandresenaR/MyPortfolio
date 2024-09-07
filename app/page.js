ddb428d70b09e234b0dcc5fb310f797d505bfca7
FandresenaR <fandresenar6@gmail.com>
2024-09-07 17:11:37 +0200

    Update favicon, add google analytics

--- app/page.js
+++ app/page.js
@@ -34,3 +34,26 @@ export default function Home() {
     </>
   )
 }
+
+// Google Analytics integration
+import Script from 'next/script'
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-VB44D6Q5MJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VB44D6Q5MJ');
+        `}
+      </Script>
+    </>
+  )
+}
