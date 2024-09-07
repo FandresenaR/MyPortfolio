01524a70f26d6d199afb28a07fdf0071682d602f
FandresenaR <fandresenar6@gmail.com>
2024-09-07 20:01:14 +0200

    setting up favicon.ico

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,23 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Fandresena Portfolio",
   description: "Fandresena Portfolio as a Web Developer, AI Developer, Pentester and Web Designer",
   icons: {
-    icon: [
-      { url: 'https://your-domain.com/favicon.ico' },
-      { url: 'https://your-domain.com/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: 'https://your-domain.com/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: { url: 'https://your-domain.com/apple-touch-icon.png' },
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
   },
-  manifest: 'https://your-domain.com/site.webmanifest',
-  themeColor: '#ffffff',
-  msTileColor: '#da532c',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
