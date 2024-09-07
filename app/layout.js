db847f9f5f50bba1c15da8323e2e4503684b1604
FandresenaR <fandresenar6@gmail.com>
2024-09-07 18:02:24 +0200

    Update favicon 4.0

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Metadata } from 'next';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,12 +8,9 @@ export const metadata = {
   title: "Fandresena Portfolio",
   description: "Fandresena Portfolio as a Web Developer, AI Developer, Pentester and Web Designer",
   icons: {
-    icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: { url: '/apple-touch-icon.png' },
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
   },
   manifest: '/site.webmanifest',
   themeColor: '#ffffff',
